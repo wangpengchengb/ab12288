356e53988b7b2d5eaae719947d20c86f99e9a999
wangpengchengb <huajiayug996@163.com>
2025-01-24 20:33:53 +0800

    add: 首页显示部分动态

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,41 +4,21 @@ const app = getApp()
  */
 Page({
   data: {
-    userInfo: {},
     isLogin: false,
-    defaultInfo: {
-      avatar_url: "../../res/image/logo.png",
-      name: "点击登录",
-      bio: "登录即可享受更多优质的服务",
-      followers: 0,
-      following: 0,
-      stared: 0,
-      watched: 0
-    }
-  },
-  comming: function () {
-    wx.showToast({
-      title: '即将上线 敬请期待'
-    });
+    friendList: [],
+    userInfo: {},
+    myList: [],
+    per_page: 10
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
     var that = this;
-    wx.navigateTo({
-      url: '../orgs/index?path=bbbug_com'
-    });
-    return;
     wx.showLoading({
       title: '数据加载中',
     });
-    that.setData({
-      userInfo: that.data.defaultInfo
-    });
-    wx.setNavigationBarTitle({
-      title: app.product.name,
-    });
+    app.loadFont();
     const updateManager = wx.getUpdateManager()
     updateManager.onUpdateReady(function () {
       wx.showModal({
@@ -52,88 +32,153 @@ Page({
     });
     updateManager.onUpdateFailed(function () {});
   },
-  onShow: function () {
-    this.getUserInfo();
+  login() {
+    app.login();
   },
-  ifNeedLogin: function () {
-    if (!this.data.isLogin) {
-      app.login();
-    } else {
-      wx.navigateTo({
-        url: '../user/motify',
-      })
-    }
-  },
-  getUserInfo: function () {
+  onPullDownRefresh() {
     var that = this;
     app.getUserInfo(function (result) {
-      wx.hideLoading();
       if (result) {
         that.setData({
-          userInfo: app.userInfo,
           isLogin: true
         });
-      } else {
-        that.setData({
-          userInfo: that.data.defaultInfo,
-          isLogin: false
+        wx.showLoading({
+          title: '更新中',
         });
+        that.getActivitys();
+      } else {
+        wx.hideLoading();
+        wx.stopPullDownRefresh();
       }
     });
   },
-  openSetting: function () {
+  onShow: function () {
+    var that = this;
+    app.getUserInfo(function (result) {
+      if (result) {
+        that.setData({
+          isLogin: true
+        });
+        that.getActivitys();
+      } else {
+        wx.hideLoading();
+        wx.stopPullDownRefresh();
+      }
+    });
+  },
+  itemClick: function (e) {
+    var that = this;
+    var menuList = [];
+    if (that.data.type == 'friends') {
+      menuList = [
+        '进入仓库', '用户信息'
+      ];
+    } else {
+      menuList = ['进入仓库'];
+    }
+    // console.log(e.mark.type);
+    // console.log(e.mark.payload);
+    switch (e.mark.type) {
+      case 'PushEvent':
+        menuList.push('查看提交');
+        break;
+      case 'IssueCommentEvent':
+        menuList.push('查看Issue评论');
+        break;
+      case 'IssueEvent':
+        menuList.push('查看Issue');
+        break;
+      case 'PullRequestEvent':
+        menuList.push('查看PullRequest');
+        break;
+      default:
+    }
+    var pathArray = e.mark.repo.split('/');
     wx.showActionSheet({
-      itemList: ['修改资料', '关于我们', '退出登录'],
-      success: function (ret) {
-        switch (ret.tapIndex) {
-          case 0:
+      itemList: menuList,
+      success: function (res) {
+        switch (menuList[res.tapIndex]) {
+          case "进入仓库":
             wx.navigateTo({
-              url: '../user/motify',
-            });
+              url: '../repos/detail?namespace=' + pathArray[0] + "&path=" + pathArray[1],
+            })
             break;
-          case 1:
+          case "用户信息":
             wx.navigateTo({
-              url: '../about/index',
-            });
+              url: '../user/detail?login=' + e.mark.login,
+            })
             break;
-          case 2:
-            var that = this;
-            wx.showModal({
-              title: '退出提醒',
-              content: '是否退出当前登录的码云帐号？',
-              showCancel: true,
-              confirmText: "退出",
-              confirmColor: "#ff4500",
-              cancelText: "返回",
-              success(res) {
-                if (res.confirm) {
-                  app.logout();
-                  app.login();
-                }
-              }
-            });
+          case "查看提交":
+            wx.navigateTo({
+              url: '../commits/detail?hash=' + e.mark.payload.after + '&namespace=' + pathArray[0] + "&path=" + pathArray[1],
+            })
+            break;
+          case "查看Issue评论":
+            wx.navigateTo({
+              url: '../issues/detail?number=' + e.mark.payload.issue.number + '&namespace=' + pathArray[0] + "&path=" + pathArray[1],
+            })
+            break;
+          case "查看Issue":
+            wx.navigateTo({
+              url: '../issues/detail?number=' + e.mark.payload.number + '&namespace=' + pathArray[0] + "&path=" + pathArray[1],
+            })
+            break;
+          case "查看PullRequest":
+            wx.navigateTo({
+              url: '../requests/commits?number=' + e.mark.payload.number + '&namespace=' + pathArray[0] + "&path=" + pathArray[1],
+            })
             break;
           default:
         }
       }
-    });
+    })
   },
-  showMyActivity: function () {
-    wx.showActionSheet({
-      itemList: ['我的通知', '我的私信'],
-      success: function (ret) {
-        switch (ret.tapIndex) {
-          case 0:
-            wx.navigateTo({
-              url: '../activity/notifications',
+  /**
+   * 获取数据列表
+   */
+  getActivitys: function (isMine = false) {
+    var that = this;
+    if (that.isGetingData) {
+      wx.hideLoading();
+      wx.stopPullDownRefresh();
+      return;
+    }
+    that.isGetingData = true;
+    var url = app.config.apiUrl + "api/v5/users/" + app.userInfo.login + "/received_events";
+    if (isMine) {
+      url = app.config.apiUrl + "api/v5/users/" + app.userInfo.login + "/events";
+    }
+    wx.request({
+      url: url,
+      method: "GET",
+      data: {
+        access_token: app.access_token,
+        per_page: that.data.per_page
+      },
+      success: function (result) {
+        that.isGetingData = false;
+        wx.hideLoading();
+        wx.stopPullDownRefresh();
+        if (result.data.hasOwnProperty("message")) {
+          wx.showModal({
+            title: '获取失败',
+            content: "你可以尝试重新登录或稍后再试",
+            showCancel: false,
+            success(res) {}
+          });
+        } else {
+          if (isMine) {
+            that.setData({
+              myList: result.data
             });
-            break;
-          case 1:
-            wx.navigateTo({
-              url: '../activity/mails',
+          } else {
+            that.setData({
+              friendList: result.data
             });
-            break;
-          default:
+          }
+          if (!isMine) {
+            that.getActivitys(true);
+          }
         }
       }
     });
@@ -141,12 +186,14 @@ Page({
   onShareAppMessage: function (res) {
     var that = this;
     if (res.from === 'button') {
-      // 来自页面内转发按钮
       console.log(res.target)
     }
-    return {
-      title: "我在码云上发现一个叫" + that.data.userInfo.name + "的大佬...",
-      path: '/pages/user/detail?login=' + that.data.userInfo.login
+    if (that.data.isLogin) {
+      return {
+        title: "我在码云上发现一个叫" + that.data.userInfo.name + "的大佬...",
+        path: '/pages/user/detail?login=' + that.data.userInfo.login,
+        imageUrl: '/res/image/share.jpg',
+      }
     }
   }
 })
